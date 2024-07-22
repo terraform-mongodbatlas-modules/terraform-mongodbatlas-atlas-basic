230df4be2393207cccaf9af3b2071e177d1ce59b
rubenVB01 <95967197+rubenVB01@users.noreply.github.com>
2024-07-22 09:39:37 +0200

    doc: Descriptions of input and output variables (#4)
    * Descriptions of input and output variables
    
    * Fix PR comments
    
    * Update variables.tf
    
    Co-authored-by: Leo Antoli <430982+lantoli@users.noreply.github.com>
    
    * Fix PR Comments
    
    * Correct format in files
    
    * Fix comments PR
    
    ---------
    
    Co-authored-by: Leo Antoli <430982+lantoli@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,19 @@
 # mongodbatlas_project
 
 output "project_id" {
-  value = mongodbatlas_project.project.id
+  description = "The project id (e.g., 65def6ce0f722a1507105bb5)."
+  value       = mongodbatlas_project.project.id
 }
 
 output "creation_timestamp" {
-  value = mongodbatlas_project.project.created
+  description = "The ISO-8601-formatted timestamp of when Atlas created the project (e.g., 2024-07-19T08:31:20Z)."
+  value       = mongodbatlas_project.project.created
 }
 
 # mongodbatlas_project_ip_access_list
 
 output "ip_ids" {
+  description = "List of objects, each containing the IP access list id and its corresponding IP address."
   value = [for i in mongodbatlas_project_ip_access_list.ip :
     {
       ip_id      = i.id
@@ -20,6 +23,7 @@ output "ip_ids" {
 }
 
 output "cidr_ids" {
+  description = "List of objects, each containing the IP access list id and its corresponding cidr block."
   value = [for c in mongodbatlas_project_ip_access_list.cidr :
     {
       cidr_id    = c.id
@@ -31,6 +35,7 @@ output "cidr_ids" {
 # mongodbatlas_database_user
 
 output "database_users" {
+  description = "List of objects, each containing the username and the user's password."
   value = [for dbu in mongodbatlas_database_user.dbuser :
     {
       username = dbu.username
@@ -43,17 +48,21 @@ output "database_users" {
 # mongodbatlas_advanced_cluster
 
 output "cluster_mongodb_version" {
-  value = mongodbatlas_advanced_cluster.cluster.mongo_db_version
+  description = "Version of MongoDB the cluster runs, in major-version.minor-version format."
+  value       = mongodbatlas_advanced_cluster.cluster.mongo_db_version
 }
 
 output "cluster_state_name" {
-  value = mongodbatlas_advanced_cluster.cluster.state_name
+  description = "Current state of the cluster. Possible values are IDLE, CREATING, UPDATING, DELETING, DELETED and REPAIRING."
+  value       = mongodbatlas_advanced_cluster.cluster.state_name
 }
 
 output "replication_specs" {
-  value = mongodbatlas_advanced_cluster.cluster.replication_specs
+  description = "Set of replication specifications for the cluster."
+  value       = mongodbatlas_advanced_cluster.cluster.replication_specs
 }
 
 output "connection_string" {
-  value = mongodbatlas_advanced_cluster.cluster.connection_strings[0].standard_srv
+  description = "Public mongodb+srv:// connection string for this cluster."
+  value       = mongodbatlas_advanced_cluster.cluster.connection_strings[0].standard_srv
 }
--- variables.tf
+++ variables.tf
@@ -1,28 +1,39 @@
 # mongodbatlas_project
 
 variable "org_id" {
-  type = string
+  description = "The ID of the organization you want to create the project within (e.g., 32b6e34b3d91647abb20e7b8)."
+  type        = string
 }
 variable "project_name" {
-  type    = string
-  default = "terraform-mongodbatlas-atlas-basic"
+  description = "The name of the project you want to create."
+  type        = string
+  default     = "terraform-mongodbatlas-atlas-basic"
 }
 
 # mongodbatlas_project_ip_access_list
 
 variable "ip_addresses" {
-  type    = list(string)
-  default = []
+  description = "List of single IP addresses to be added to the IP access list."
+  type        = list(string)
+  default     = []
 }
 
 variable "cidr_blocks" {
-  type    = list(string)
-  default = []
+  description = "List of ranges of IP addresses in CIDR notation to be added to the IP access list."
+  type        = list(string)
+  default     = []
 }
 
 # mongodbatlas_database_user
 
 variable "database_users" {
+  description = <<EOT
+                Database user information. Specifically:
+                  -username: Username for authenticating to MongoDB.
+                  -password: User's initial password.
+                  -roles: List of user's roles and the databases / collections on which the roles apply.
+                  -scopes: Array of clusters and Atlas Data Lakes that this user has access to. If omitted, Atlas grants the user access to all the clusters and Atlas Data Lakes in the project by default. 
+                EOT
   type = list(object({
     username = string
     password = string
@@ -41,26 +52,31 @@ variable "database_users" {
 # mongodbatlas_advanced_cluster
 
 variable "cluster_name" {
-  type    = string
-  default = "Cluster0"
+  description = "Name of the cluster as it appears in Atlas."
+  type        = string
+  default     = "Cluster0"
 }
 
 variable "provider_name" {
-  type    = string
-  default = "AWS"
+  description = "Cloud service provider on which the servers are provisioned. Possible values are AWS, GCP, AZURE or TENANT."
+  type        = string
+  default     = "AWS"
 }
 
 variable "backing_provider_name" {
-  type    = string
-  default = "AWS"
+  description = "Cloud service provider on which you provision the host for a multi-tenant cluster."
+  type        = string
+  default     = "AWS"
 }
 
 variable "region_name" {
-  type    = string
-  default = "US_EAST_1"
+  description = "Physical location of your MongoDB cluster (more information on https://www.mongodb.com/docs/atlas/cloud-providers-regions/)."
+  type        = string
+  default     = "US_EAST_1"
 }
 
 variable "electable_specs" {
+  description = "Hardware specifications for electable nodes in the region. If the provider is TENANT, instance size should be M5 or lower."
   type = object({
     instance_size = string
     node_count    = optional(number, 3)
@@ -72,6 +88,7 @@ variable "electable_specs" {
 }
 
 variable "analytics_specs" {
+  description = "Hardware specifications for analytics nodes needed in the region."
   type = object({
     instance_size = string
     node_count    = optional(number, 1)
